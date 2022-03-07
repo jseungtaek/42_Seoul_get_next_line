f3ab03e7f347be010c1f86cc596120dadf7eb07e
Seungtaek Jeon <sejeon@c1r9s7.42seoul.kr>
2022-03-07 19:45:42 +0900

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sejeon <sejeon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 14:14:53 by sejeon            #+#    #+#             */
-/*   Updated: 2022/03/07 19:31:52 by sejeon           ###   ########.fr       */
+/*   Updated: 2022/03/07 19:45:33 by sejeon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,17 @@ static void ft_free(char **str)
     }
 }
 
+static char all_return(int fd, char **buf, char **backup, int bytes)
+{
+
+}
+
 char	get_next_line(int fd)
 {
     static char	*backup[OPEN_MAX + 1];
     char		*buf;
     char		*res;
+    int         bytes;
     
     if (fd < 0 || fd > OPEN_MAX || BUFFER_SIZE <= 0)
         return (NULL);
@@ -34,5 +40,4 @@ char	get_next_line(int fd)
         return (NULL);
     if (!backup[fd])
         *backup = ft_strdup("");
-
 }
