93e58ce5cc61b7feb9b42dd0c897dd588b643269
jseungtaek <tmdxor30@gmail.com>
2022-03-10 20:51:51 +0900

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -12,32 +12,72 @@
 
 #include "get_next_line.h"
 
-static void ft_free(char **str)
+static void	ft_free(char **str)
 {
-    while (*str)
-    {
-        free(*str);
-        *str = NULL;
-    }
+	while (*str)
+	{
+		free(*str);
+		*str = NULL;
+	}
 }
 
-static char all_return(int fd, char **buf, char **backup, int bytes)
+static char	*return_next_line(char **backup)
 {
+	char	*line;
+	char	*pre_backup;
+	int		index;
 
+	index = 0;
+	line = NULL;
+	while ((*backup)[index] != '\n' && (*backup)[index])
+		index++;
+	if ((*backup)[index] == '\n')
+	{
+		pre_backup = *backup;
+		line = ft_substr(pre_backup, 0, index + 1);
+		*backup = ft_strdup(&(*backup)[index + 1]);
+		free(pre_backup);
+		return (line);
+	}
+	line = ft_strdup(*backup);
+	ft_free(backup);
+	return (line);
 }
 
-char	get_next_line(int fd)
+static char	*check_all(char **backup, int bytes, int fd)
 {
-    static char	*backup[OPEN_MAX + 1];
-    char		*buf;
-    char		*res;
-    int         bytes;
-    
-    if (fd < 0 || fd > OPEN_MAX || BUFFER_SIZE <= 0)
-        return (NULL);
-    buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
-    if (!buf)
-        return (NULL);
-    if (!backup[fd])
-        *backup = ft_strdup("");
+	if (bytes < 0)
+		return (NULL);
+	if (!bytes && (!backup[fd] || !*backup[fd]))
+		return (NULL);
+	return (return_next_line(backup));
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*backup[256 + 1];
+	char		*buf;
+	char		*pre_backup;
+	int			bytes;
+
+	if (fd < 0 || fd > 256 || BUFFER_SIZE <= 0)
+		return (NULL);
+	buf = (char *)malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!buf)
+		return (NULL);
+	if (!backup[fd])
+		backup[fd] = ft_strdup("");
+	bytes = 1;
+	while (!ft_strchr(*backup, '\n') && bytes)
+	{
+		bytes = read(fd, buf, BUFFER_SIZE);
+		if (bytes == -1)
+			break ;
+		buf[bytes] = '\0';
+		pre_backup = *backup;
+		*backup = ft_strjoin(pre_backup, buf);
+		free(pre_backup);
+	}
+	free(buf);
+	return (check_all(backup, bytes, fd));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -10,11 +10,21 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE.H
-# define GET_NEXT_LINE.H
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <unistd.h>
 # include <stdlib.h>
-# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(const char *s1, const char *s2);
+size_t	ft_strlen(const char *s);
 
 #endif
